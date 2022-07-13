b82f845204b40e2e80dc1cc1656b7d5b1da070ef
HalFikk <loktaev95@mail.ru>
2022-07-13 16:52:40 +0300

    С защитой от SQL-инъекций

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -39,9 +39,12 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String query = "INSERT INTO user(Name, LastName, Age) VALUES(?, ?, ?);";
         try (PreparedStatement statement = Util.getConnection()
-                .prepareStatement("INSERT INTO user(Name, LastName, Age)\n" + "VALUES('"
-                        + name + "', '" + lastName + "', " + age + ");")) {
+                .prepareStatement(query)) {
+            statement.setString(1, name);
+            statement.setString( 2, lastName);
+            statement.setByte(3, age);
             statement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Не удалось сохранить пользователя!");
@@ -49,8 +52,10 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
+        String query = "DELETE FROM user WHERE ID = ?;";
         try (PreparedStatement statement = Util.getConnection()
-                .prepareStatement("DELETE FROM user WHERE ID = '" + id + "';")) {
+                .prepareStatement(query)) {
+            statement.setLong(1, id);
             statement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Не удалось удалить пользователя!");
